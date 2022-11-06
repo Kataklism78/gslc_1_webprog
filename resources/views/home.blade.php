02c8f496da5d7c793aafe3ff398c2fc9c82e4024
Mauricio Felix Tan <felixtan1302@gmail.com>
2022-11-06 12:04:35 +0700

    Added gslc2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,15 +8,24 @@
 
 <h1>Hello World</h1>
   
-@if (count($datas) === 1)
+<div class= "bg-black" style="height: 500px">
+    <div class="" >
+        
+        <img src="{{ $img }}" class="img-fluid" alt="...">
+        @for ($i = 1; $i <= 5; $i++)   
 
-Kamu punya data 1 
-    
-@elseif(count($datas) === 3)
-
-kamu punya data 3  
-    
-@endif
+        <div class="text-center text-white bg-primary rounded-5 m-2 pt-3" style="">
+           
+            <h2>Anda sedang berada di dalam loop ke {{ $i }}</h2>
+            @if ($i != 5)
+                <h2>Selanjutnya adalah {{$i+1}}</h2>
+            @elseif ($i == 5)
+                <h4>Anda sudah tiba di loop terakhir</h4>
+            @endif    
+        </div>
+    @endfor
+    </div>
+</div>
 
     
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
-    
-    $datas = [1,2,3];
 
-    return view('home', compact('datas'));
-});
 
+Route::get('/', [TestController::class,'test']);
 
